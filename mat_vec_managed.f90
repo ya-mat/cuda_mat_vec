419a074af9eb29b575fcf200b16e64e856915dba
ya-mat <none>
2024-10-08 18:01:27 +0900

    OK

--- mat_vec_managed.f90
+++ mat_vec_managed.f90
@@ -28,17 +28,22 @@ program matrix_vector_multiplication
 
     integer :: N
     integer :: mode
-    complex*16, allocatable :: A(:,:), x(:), y(:)
+    complex*16, allocatable :: A(:,:), x(:), y(:), x2(:, :), y2(:, :)!, Arow(:)
+    complex*16, device, allocatable :: d_A(:, :)
     integer :: i, j
-    real :: time0, time1, time2
+    real :: time0, time1, time2, time3
 
     print *, 'please input N, mode:'
     read *, N, mode
     print *, 'N:', N, 'mode:', mode
 
     allocate(A(N, N))
+    allocate(d_A(N, N))
     allocate(x(N))
     allocate(y(N))
+    allocate(x2(N, N))
+    allocate(y2(N, N))
+!    allocate(Arow(N*N))
 
     call cpu_time(time0)
 
@@ -54,14 +59,20 @@ program matrix_vector_multiplication
             call random_number(tmp)
             call random_number(tmp2)
             A(i, j) = dcmplx(tmp * 2.0d0 - 1.0d0, tmp2 * 2.0d0 - 1.0d0)
+            !Arow(j + (i - 1)*N) = A(i, j)
+            x2(i, j) = dcmplx(tmp2 * 2.0d0 - 1.0d0, tmp * 2.0d0 - 1.0d0)
          end do
       end do
     end block
 
+    d_A = A
+
     call cpu_time(time1)
     if(mode == 0) then
        ! cublas
-       call cublasZgemv('N', N, N, dcmplx(1.0d0, 0.0d0), A, N, x, 1, dcmplx(0.0d0, 0.0d0), y, 1)
+       !call cublasZgemv('N', N, N, dcmplx(1.0d0, 0.0d0), A, N, x, 1, dcmplx(0.0d0, 0.0d0), y, 1)
+       call cublasZgemv('N', N, N, dcmplx(1.0d0, 0.0d0), d_A, N, x, 1, dcmplx(0.0d0, 0.0d0), y, 1)
+       !call cublasZgemv('N', N, N, dcmplx(1.0d0, 0.0d0), Arow, N, x, 1, dcmplx(0.0d0, 0.0d0), y, 1)
        i = cudaDeviceSynchronize()
     else if(mode == 1) then
        ! kernel
@@ -77,9 +88,27 @@ program matrix_vector_multiplication
 
     print *, 'Result y(1:10):', y(1:10)
 
+    if(mode == 0) then
+       ! cublas
+!       call cublasZgemm('N', 'N', N, N, N, dcmplx(1.0d0, 0.0d0), A, N, x2, N, dcmplx(0.0d0, 0.0d0), y2, N)
+       call cublasZgemm('N', 'N', N, N, N, dcmplx(1.0d0, 0.0d0), d_A, N, x2, N, dcmplx(0.0d0, 0.0d0), y2, N)
+       i = cudaDeviceSynchronize()
+    else if(mode == 1) then
+       ! kernel
+       write(*,*) 'mode == 1 for gemm is none'
+    else if(mode == 2) then
+       ! cpu
+       if(N <= 2000) y2 = matmul(A, x2)
+    end if
+    call cpu_time(time3)
+
+    print *, 'Result y2(1:10, 1):', y2(1:10, 1)
+
     print *, 'init time:', time1 - time0, 'seconds'
     if(mode == 0) print *, 'CuBlas GPU mat_vec_time (CPU time):', time2 - time1, 'seconds'
-    if(mode == 1) print *, 'GPU mat_vec_time (CPU time):', time2 - time1, 'seconds'
-    if(mode == 2) print *, 'CPU mat_vec_time (CPU time):', time2 - time1, 'seconds'
+    if(mode == 1) print *, 'GPU mat_vec_time (CPU time):',        time2 - time1, 'seconds'
+    if(mode == 2) print *, 'CPU mat_vec_time (CPU time):',        time2 - time1, 'seconds'
+    if(mode == 0) print *, 'CuBlas GPU mat_mat_time (CPU time):', time3 - time2, 'seconds'
+    if(mode == 2) print *, 'CPU mat_mat_time (CPU time):',        time3 - time2, 'seconds'
 
 end program matrix_vector_multiplication
